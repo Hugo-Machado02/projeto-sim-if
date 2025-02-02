c32a33324cfd055905e761368041d4718f7077fd
GarotosDePr0grama <shayrakellyeleoterio@gmail.com>
2025-02-02 01:04:07 -0300

    ✨ feat: Adiciona classe Cidade

---
+++ src/models/Cidade.py
@@ -0,0 +1,14 @@
+class Cidade:
+    def __init__(self, nome, corredor, listaBlocos):
+        self.__nome = nome
+        self.__corredorPrincipal = corredor
+        self.__listaBlocos = listaBlocos
+    
+    def getnome(self):
+        return self.__nome
+    
+    def getcorredorPrincipal(self):
+        return self.__corredorPrincipal
+    
+    def getcorredorPrincipal(self):
+        return self.__listaBlocos
