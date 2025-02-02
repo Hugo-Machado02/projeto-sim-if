5128e01e173d5ef552ed3595b81507f540bbbeb9
Hugo Machado <hugo_machado02@hotmail.com>
2025-02-02 01:11:26 -0300

    ✅ test: realização dos testes das classes e métodos

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
-from models.Simulacao import Simulacao
-from controller import criaPessoa, criaSalas
+from models.Pessoa import Pessoa
+from controller import criaPessoa, criaBloco
 from dotenv import load_dotenv
 import os
+import time
 
 load_dotenv()
 NUM_BLOCOS = int(os.getenv("NUM_BLOCOS"))
@@ -11,11 +12,34 @@ TEMPO_EXECUCAO = int(os.getenv("TEMPO_EXECUCAO"))
 
 print(f"Teste -> Blocos: {NUM_BLOCOS}; Salas: {NUM_SALAS}; Pessoas: {NUM_PESSOAS}; Execucao: {TEMPO_EXECUCAO};" )
 
-salas = criaSalas.criacaoSalas(NUM_SALAS)
+bloco = criaBloco.geraBlocos(NUM_BLOCOS, NUM_SALAS)
 
 pessoas = criaPessoa.criacaoPessoas(NUM_PESSOAS)
 
-sim = Simulacao(TEMPO_EXECUCAO, pessoas, salas)
+for p in pessoas:
+    decisaoBloco = p.decisao(bloco)
+    decisaoSala = p.decisao(decisaoBloco.getListaSalas())
+    if decisaoSala.entrar(p):
+        print(f'A {p.getNome()} Decidiu entrar no {decisaoBloco.getNome()} na {decisaoSala.getNome()}')
+    else:
+        decisaoBloco.getCorredor().adicionarPessoa(p)
+        print(f"A Pessoa {p.getNome()} entrou no Corredor")
+    time.sleep(1)
 
-sim.iniciaSimulacao()
+for i in bloco:
+    print('\n\n---------------------')
+    print(f"{i.getNome()}")
+    salas = i.getListaSalas()
+    print(f"-> Corredor = {len(i.getCorredor().getListaPessoas())} Pessoas")
+    for x in salas:
+        print(f"-> {x.getNome()} = {x.quantidadePessoas()} Pessoas")
+        time.sleep(1)
+        
+
+
+# pessoas = criaPessoa.criacaoPessoas(NUM_PESSOAS)
+
+# sim = Simulacao(TEMPO_EXECUCAO, pessoas, salas)
+
+# sim.iniciaSimulacao()
 print("Fim da simulação!")
