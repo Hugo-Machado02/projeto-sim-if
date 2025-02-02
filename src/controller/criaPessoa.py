acc8d9a34d05fda2ce01fb386e38f807fa19867e
Hugo Machado <hugo_machado02@hotmail.com>
2025-02-02 00:50:52 -0300

    ♻️ Refactor: Refatora criação das Pessoas

--- src/controller/criaPessoa.py
+++ src/controller/criaPessoa.py
@@ -4,6 +4,6 @@ listaPessoas = []
 
 def criacaoPessoas(num):
     for i in range(num):
-        pessoa = Pessoa(f"Pessoa {i + 1}", f"local {i + 1}", f"Local {i + 1}")
+        pessoa = Pessoa(f"Pessoa", f"{i + 1}")
         listaPessoas.append(pessoa)
     return listaPessoas
