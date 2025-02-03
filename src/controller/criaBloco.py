8d7cbaa8ed86a3d2be9d834e0c1630c480e5daf7
Hugo Machado <hugo_machado02@hotmail.com>
2025-02-02 23:43:21 -0300

    ♻️ refactor: Refatora informações de criação dos Blocos

--- src/controller/criaBloco.py
+++ src/controller/criaBloco.py
@@ -2,14 +2,12 @@ from models.Bloco import Bloco
 from models.Corredor import Corredor
 from controller.criaSalas import criacaoSalas
 nomes = ["Informatica", "Quimica", "Pedagogia", "Zootecnia", "Alimentos", "Núcleo Comum", "Administração", "API"]
-blocos = []
 
 def geraBlocos(numBlocos, numSalas):
+    blocos = []
     for i in range(numBlocos):
-        nome = f"Bloco - {nomes[i]}"
         salas = criacaoSalas(numSalas)
         corredor = Corredor()
-        bloco = Bloco(nome, corredor, salas)
+        bloco = Bloco(nomes[i], corredor, salas)
         blocos.append(bloco)
-
     return blocos
