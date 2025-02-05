4b0e56808dde3c0206250abeac583b613cd9f69b
Hugo Machado <hugo_machado02@hotmail.com>
2025-02-04 23:45:20 -0300

    ✨ feat:  Adiciona método de impressão das Classes

--- src/utils/operacoes.py
+++ src/utils/operacoes.py
@@ -5,3 +5,20 @@ def enviaLocal(pessoa, localAtual, ListaLocais):
         destino.adicionaPessoa(pessoa)
 
 
+#Verifica se o usuário quer continuar no local
+def continuarLocal(pessoa):
+    return pessoa.getDecisao([True, False])
+
+
+def imprimirCidade(cidade):
+    corredorPrincipal = cidade.getCorredor()
+    blocos = cidade.getlistaBlocos()
+
+    print(f"\n\nCidade: {cidade.getNome()}")
+    print(f"->Corredor Principal -> {corredorPrincipal.getQuantidadePessoas()} Pessoas")
+    for b in blocos:
+        print(f"\t-> Bloco - {b.getNome()}")
+        salas = b.getListaSalas()
+        print(f"\t\t-> Corredor de Bloco: {b.getCorredor().getQuantidadePessoas()} Pessoas")
+        for sala in salas:
+            print(f"\t\t-> Sala: {sala.getQuantidadePessoas()} Pessoas")
