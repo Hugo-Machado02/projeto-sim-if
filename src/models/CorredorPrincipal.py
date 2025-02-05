1e8b71bb926be71cad1bd42c05c7f74b01047c13
GarotosDePr0grama <shayrakellyeleoterio@gmail.com>
2025-02-04 23:27:45 -0300

    ✨ feat: Adiciona classe CorredorPrincipal
    Classe que irá herdar de Corredor.

---
+++ src/models/CorredorPrincipal.py
@@ -0,0 +1,27 @@
+from models.Corredor import Corredor
+from utils.operacoes import continuarLocal, enviaLocal, imprimirCidade
+import time
+
+class CorredorPrincipal(Corredor):
+
+    def __init__(self, capacidade):
+        super().__init__(capacidade)
+
+    def incluiPessoas(self, listaPessoas):
+        for pessoa in listaPessoas:
+            self.adicionaPessoa(pessoa)
+
+    # Implementação do método abstrato
+    def executaCorredor(self, cidade, listaDestinos):
+        while True:
+            pessoas = self.getListaPessoas()
+            if pessoas:
+                for pessoa in pessoas:
+                    if not continuarLocal(pessoa):
+                        enviaLocal(pessoa, cidade.getCorredor(), listaDestinos)
+                        time.sleep(1)
+                    imprimirCidade(cidade)
+            else:
+                print("Näo Possui mais Pessoas no Corredor principal")
+                time.sleep(1)
+                break
