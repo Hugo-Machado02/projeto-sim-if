9bc50575154596f8ead70d7fcb792b861e89711e
Hugo Machado <hugo_machado02@hotmail.com>
2025-02-04 23:33:22 -0300

    ✨ feat: Adiciona método de operações dentro do Bloco

--- src/models/Bloco.py
+++ src/models/Bloco.py
@@ -1,3 +1,5 @@
+from utils.operacoes import imprimirCidade
+import time
 class Bloco:
     
     # Construtor
@@ -15,6 +17,9 @@ class Bloco:
     
     def getListaSalas(self):
         return self.__listaSalas
+    
+    def adicionaPessoa(self, pessoa):
+        self.getCorredor().adicionaPessoa(pessoa)
          
     # Funções
     def adicionaSala(self, sala):
@@ -25,3 +30,14 @@ class Bloco:
             self.__listaSalas.remove(sala)
         else:
             print("Não há nenhuma sala!")
+
+    #Funcão de Ação da Classe execucaoBloco
+    def execucaoBlocos(self, cidade):
+        corredorBloco = self.getCorredor()
+        salas = self.getListaSalas()
+        # for sala in salas:
+        #     sala.executaSalas(sala, corredorBloco)
+        #for responsável pelo corredor
+        salas.append(cidade.getCorredor())
+        self.getCorredor().executaCorredor(cidade, corredorBloco, salas)
+        time.sleep(1)
