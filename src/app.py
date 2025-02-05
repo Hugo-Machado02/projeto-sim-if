d543f7d651f53e39f4cd27a3541845db5a80dda9
Hugo Machado <hugo_machado02@hotmail.com>
2025-02-05 03:11:53 -0300

    :white_check_mark: test: Testes de Funcionamento do Código

--- src/app.py
+++ src/app.py
@@ -1,45 +1,40 @@
-from models.Pessoa import Pessoa
-from controller import criaPessoa, criaBloco
+from controller.geraElementos import cricacaoElementos
+from controller.criaPessoa import criacaoPessoas
+from utils.operacoes import imprimirCidade
 from dotenv import load_dotenv
 import os
 import time
 
+#Carrega Variaveis de ambiente
 load_dotenv()
 NUM_BLOCOS = int(os.getenv("NUM_BLOCOS"))
 NUM_SALAS = int(os.getenv("NUM_SALAS"))
 NUM_PESSOAS = int(os.getenv("NUM_PESSOAS"))
 TEMPO_EXECUCAO = int(os.getenv("TEMPO_EXECUCAO"))
 
-print(f"Teste -> Blocos: {NUM_BLOCOS}; Salas: {NUM_SALAS}; Pessoas: {NUM_PESSOAS}; Execucao: {TEMPO_EXECUCAO};" )
+cidade = cricacaoElementos(NUM_BLOCOS, NUM_SALAS, NUM_PESSOAS)
+cont = 1
+listaPersonalizada = []
+listaPersonalizada2 = []
+for bloco in cidade.getlistaBlocos():
+    listaPersonalizada.append(bloco)
 
-bloco = criaBloco.geraBlocos(NUM_BLOCOS, NUM_SALAS)
-
-pessoas = criaPessoa.criacaoPessoas(NUM_PESSOAS)
-
-for p in pessoas:
-    decisaoBloco = p.decisao(bloco)
-    decisaoSala = p.decisao(decisaoBloco.getListaSalas())
-    if decisaoSala.entrar(p):
-        print(f'A {p.getNome()} Decidiu entrar no {decisaoBloco.getNome()} na {decisaoSala.getNome()}')
-    else:
-        decisaoBloco.getCorredor().adicionarPessoa(p)
-        print(f"A Pessoa {p.getNome()} entrou no Corredor")
+corredorPrincipal = cidade.getCorredor()
+while True:
+    print(cont)
     time.sleep(1)
+    corredorPrincipal.executaCorredor(cidade, listaPersonalizada)
 
-for i in bloco:
-    print('\n\n---------------------')
-    print(f"{i.getNome()}")
-    salas = i.getListaSalas()
-    print(f"-> Corredor = {len(i.getCorredor().getListaPessoas())} Pessoas")
-    for x in salas:
-        print(f"-> {x.getNome()} = {x.quantidadePessoas()} Pessoas")
-        time.sleep(1)
+    for b in cidade.getlistaBlocos():
+        for s in b.getListaSalas():
+            listaPersonalizada2.append(s)
+        listaPersonalizada2.append(cidade.getCorredor())
+        b.execucaoBlocos(cidade, listaPersonalizada2)
         
+    if cont == 4:
+        break
+    cont = cont + 1
 
 
-# pessoas = criaPessoa.criacaoPessoas(NUM_PESSOAS)
-
-# sim = Simulacao(TEMPO_EXECUCAO, pessoas, salas)
-
-# sim.iniciaSimulacao()
+imprimirCidade(cidade)
 print("Fim da simulação!")
