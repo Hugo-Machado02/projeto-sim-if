2a8a0bc0e87597d5911355907369931032a18b86
Hugo Machado <hugo_machado02@hotmail.com>
2025-02-04 23:32:37 -0300

    ✨ feat: Adiciona instanciação das Pessoas
    Insere as pessoas que foram criadas dentro do corredor

--- src/controller/geraElementos.py
+++ src/controller/geraElementos.py
@@ -1,13 +1,10 @@
-from controller import criaCidade, criaBloco, criaSalas, criaPessoa
+from controller import criaCidade, criaSalas, criaPessoa, criaBloco
 
 #Realiza a criacão dos elementos para a simulacao
 def cricacaoElementos(numeroBlocos, numeroSalas, numeroPessoas):
-
     blocos = criaBloco.geraBlocos(numeroBlocos, numeroSalas)
     cidade = criaCidade.criacaoCidade(blocos)
     pessoas = criaPessoa.criacaoPessoas(numeroPessoas)
+    cidade.getCorredor().incluiPessoas(pessoas)
 
-    for p in pessoas:
-        cidade.getCorredor().adicionaPessoa(p)
-        print(f"{p.getNome()} Entrando no corredor da cidade - {cidade.getNome()}")
     return cidade
