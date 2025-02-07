229655e228c6c51623050ecacca7eeeac6a9f9f6
Hugo Machado <hugo_machado02@hotmail.com>
2025-02-07 19:52:40 -0300

    ♻️ refactor: Limpa elementos não usados

--- src/controller/geraElementos.py
+++ src/controller/geraElementos.py
@@ -3,14 +3,9 @@ import time
 
 #Realiza a criacão dos elementos para a simulacao
 def cricacaoElementos(numeroBlocos, numeroSalas, numeroPessoas, maxPessoasCorredor, maxPessoasSalas):
-    print("Gerando elementos...")
     blocos = criaBloco.geraBlocos(numeroBlocos, numeroSalas, maxPessoasCorredor, maxPessoasSalas)
     cidade = criaCidade.criacaoCidade(blocos)
     pessoas = criaPessoa.criacaoPessoas(numeroPessoas)
-    time.sleep(1.5)   
-
-    print("Inserindo Pessoas no Corredor Principal")
     cidade.getCorredor().incluiPessoas(pessoas)
-    time.sleep(1.5)
 
     return cidade
