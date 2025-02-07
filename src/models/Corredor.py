edbe117a77aa50c27300cfdc2892e1666705ca2b
otuti Code <arthur.renato.nv@gmail.com>
2025-02-07 00:28:38 -0300

    ✨ feat: Adiciona atributo nome
    Adiciona atributo nome e encapsulamento

--- src/models/Corredor.py
+++ src/models/Corredor.py
@@ -2,11 +2,15 @@ from abc import ABC, abstractmethod
 
 class Corredor(ABC):
     
-    def __init__(self, capacidade):
+    def __init__(self, nome, capacidade):
+        self.__nome = nome
         self.__capacidade = capacidade
         self.__listaPessoas = []
         
     # Encapsulamento
+    def getNome(self):
+        return self.__nome
+
     def getCapacidade(self):
         return self.__capacidade
     
