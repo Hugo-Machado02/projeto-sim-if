a50d92e2120a936705deac6e31d2d95356aad026
Hugo Machado <hugo_machado02@hotmail.com>
2025-02-07 17:10:35 -0300

    ✨ feat: Inicia o Servidor Flask

--- src/app.py
+++ src/app.py
@@ -1,35 +1,30 @@
+import time, os, threading
+from queue import Queue, Empty
 from controller.geraElementos import cricacaoElementos
-from controller.criaPessoa import criacaoPessoas
-from utils.operacoes import imprimirCidade
 from dotenv import load_dotenv
-import os
-import time
+from flask import Flask
 
-#Carrega Variaveis de ambiente
+# Carrega Variáveis de ambiente
 load_dotenv()
 NUM_BLOCOS = int(os.getenv("NUM_BLOCOS"))
 NUM_SALAS = int(os.getenv("NUM_SALAS"))
 NUM_PESSOAS = int(os.getenv("NUM_PESSOAS"))
+MAX_PESSOAS_CORREDOR = int(os.getenv("MAX_PESSOAS_CORREDOR"))
+MAX_PESSOAS_SALAS = int(os.getenv("MAX_PESSOAS_SALAS"))
+THREADS = int(os.getenv("THREADS"))  # Máximo de threads simultâneas
 TEMPO_EXECUCAO = int(os.getenv("TEMPO_EXECUCAO"))
 
-cidade = cricacaoElementos(NUM_BLOCOS, NUM_SALAS, NUM_PESSOAS)
-cont = 1
-listaPersonalizada = []
-listaPersonalizada2 = []
-for bloco in cidade.getlistaBlocos():
-    listaPersonalizada.append(bloco)
+app = Flask(__name__)
+fimThreads = threading.Event()
+semaforo = threading.Semaphore(THREADS)
+filaTarefas = Queue()
 
-corredorPrincipal = cidade.getCorredor()
-timeInicial = time.time();
 
-while time.time() - timeInicial < TEMPO_EXECUCAO:
-    corredorPrincipal.executaCorredor(cidade, listaPersonalizada)
+if __name__ == '__main__':
+    # Iniciar servidor Flask em uma thread separada
+    flaskThread = threading.Thread(target=app.run, kwargs={'debug': False, 'threaded': True})
+    flaskThread.start()
 
-    for b in cidade.getlistaBlocos():
-        for s in b.getListaSalas():
-            listaPersonalizada2.append(s)
-        listaPersonalizada2.append(cidade.getCorredor())
-        b.execucaoBlocos(cidade, listaPersonalizada2)
-
-imprimirCidade(cidade)
-print("Fim da simulação!")
+    # Finalizar servidor Flask
+    print("Finalizando servidor Flask...")
+    os._exit(0)
