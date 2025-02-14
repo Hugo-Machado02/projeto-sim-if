c4f15d4107c65b2051bb5925fb3d847e56e63198
Hugo Machado <hugo_machado02@hotmail.com>
2025-02-14 01:33:15 -0300

    ✨ feat: Adiciona remoção da pessoa da cidade

--- src/models/CorredorPrincipal.py
+++ src/models/CorredorPrincipal.py
@@ -16,26 +16,11 @@ class CorredorPrincipal(Corredor):
         listagem = listaDestinos
         pessoa = selecionaPessoa(self.getListaPessoas())
         if not continuarLocal(pessoa):
-            enviaLocal(pessoa, self, listagem)
-            
-    def processarPessoas(self, url_cidade=None):
-        pessoas = self.getListaPessoas()
-        
-        for pessoa in pessoas[:]:
-            if pessoa.decisaoSaida():
-                print(f"{pessoa.getNome()} decidiu sair da cidade.")
-                if url_cidade:
-                    if self.enviarParaCidade(pessoa, url_cidade):
-                        self.removePessoa(pessoa)
+            continuaCidade = continuarLocal(pessoa)
+            if(continuaCidade == True):
+                enviaLocal(pessoa, self, listagem)
+                return False
             else:
-                print(f"{pessoa.getNome()} decidiu ficar na cidade.")
-
-    def enviarParaCidade(self, pessoa, url_cidade):
-        try:
-            response = requests.post(f"http://{url_cidade}/receberPessoa", json={"nome": pessoa.getNome()})
-            if response.status_code == 200:
-                print(f"{pessoa.getNome()} foi enviado para outra cidade.")
-            else:
-                print(f"Falha ao enviar {pessoa.getNome()}.")
-        except Exception as e:
-            print(f"Erro ao enviar {pessoa.getNome()}: {e}")
+                self.removePessoa(pessoa)
+                return pessoa.getNome()
+        return False
