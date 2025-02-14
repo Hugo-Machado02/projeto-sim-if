360aeddd48eeecc2836fe4e27bcce3a70de35f37
Hugo Machado <hugo_machado02@hotmail.com>
2025-02-14 13:26:04 -0300

    ♻️ refactor: Altera passagem de parametro

--- src/controller/criaCidade.py
+++ src/controller/criaCidade.py
@@ -4,5 +4,5 @@ from models.CorredorPrincipal import CorredorPrincipal
 
 def criacaoCidade(listaBlocos):
     corredor = CorredorPrincipal("Corredor Principal", 40)
-    cidade = Cidade("Sim-IF", corredor, listaBlocos)
+    cidade = Cidade("SymIF - Hugo", corredor, listaBlocos)
     return cidade
