3b50e406d078892fedaa043314d8fd54e50fbc73
Hugo Machado <hugo_machado02@hotmail.com>
2025-02-14 13:27:51 -0300

    ✨ feat: Adiciona função de criação de novo associado

--- src/controller/criaPessoa.py
+++ src/controller/criaPessoa.py
@@ -4,6 +4,10 @@ listaPessoas = []
 
 def criacaoPessoas(numeroPessoas):
     for i in range(numeroPessoas):
-        pessoa = Pessoa(f"Pessoa", f"{i + 1}")
+        pessoa = Pessoa(f"Pessoa", f"{i + 1}", "Natural")
         listaPessoas.append(pessoa)
     return listaPessoas
+
+def criacaoPessoasCidades(nome, cidade):
+        pessoa = Pessoa(nome, f" - {cidade}", cidade)
+        return pessoa
